bbbc870adc777d891270ad5d65d33c8a4e9bb543
Collin Nolen <collinnolen@gmail.com>
2022-12-12 00:21:59 -0800

    mend

--- LadderTrackerLauncher/VersionManager.cs
+++ LadderTrackerLauncher/VersionManager.cs
@@ -1,18 +1,14 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Net;
-using System.Net.Http;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace LadderTrackerLauncher
 {
     internal class VersionManager
     {
         const string EXE_NAME = "LadderTracker.exe";
+        private static List<string> FILE_DELETION_EXCEPTIONS = new List<string>(){ "LadderTrackerDB.db" };
 
         public static void UpdateAndLaunch()
         {
@@ -21,7 +17,7 @@ namespace LadderTrackerLauncher
 
             if(IsNewerVersion(currentVersion, latestVersion))
             {
-                FileUtils.ClearInstallDirectory(new List<string>() { "LadderTrackerDB.db" });
+                FileUtils.ClearInstallDirectory(FILE_DELETION_EXCEPTIONS);
                 GithubHttpClient.DownloadVersion(latestVersion, FileUtils.GetInstallDirectory());
                 FileUtils.Unzip(latestVersion);
             }
@@ -29,24 +25,34 @@ namespace LadderTrackerLauncher
             Process.Start(Path.Combine(FileUtils.GetInstallDirectory(), EXE_NAME));
         }
 
-        static bool IsNewerVersion(string originalVersion, string newVersion)
+        static bool IsNewerVersion(string originalVersion, string incomingVersion)
         {
             if (originalVersion == null)
                 return true;
 
-            if (originalVersion == newVersion)
+            if (originalVersion == incomingVersion)
                 return false;
 
+            //regex magic to pull just the x.x.x numbering
             string origExtracted = Regex.Match(originalVersion, @"(\d+\.\d+\.\d+)").Groups[0].Value;
-            string newExtracted = Regex.Match(newVersion, @"(\d+\.\d+\.\d+)").Groups[0].Value;
+            string newExtracted = Regex.Match(incomingVersion, @"(\d+\.\d+\.\d+)").Groups[0].Value;
 
-            string[] firstVersions = origExtracted.Split('.');
-            string[] secondVersions = newExtracted.Split('.');
+            string[] originalVersionNumbers = origExtracted.Split('.');
+            string[] incomingVersionNumbers = newExtracted.Split('.');
 
-            int firstNumerical = (int.Parse(firstVersions[0]) * 100) + (int.Parse(firstVersions[1]) * 10) + int.Parse(firstVersions[2]);
-            int secondNumerical = (int.Parse(secondVersions[0]) * 100) + (int.Parse(secondVersions[1]) * 10) + int.Parse(secondVersions[2]);
+            //major
+            if (int.Parse(originalVersionNumbers[0]) < int.Parse(incomingVersionNumbers[0]))
+                return true;
 
-            return firstNumerical < secondNumerical;
+            //minor
+            if (int.Parse(originalVersionNumbers[1]) < int.Parse(incomingVersionNumbers[1]))
+                return true;
+
+            //revision
+            if (int.Parse(originalVersionNumbers[2]) < int.Parse(incomingVersionNumbers[2]))
+                return true;
+
+            return false;
         }
     }
 }
